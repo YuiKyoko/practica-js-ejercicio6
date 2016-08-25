d323899abbdb9ee684003e6682bf874b5206473b
YuiKyoko <kimberlyesquivelgala@gmail.com>
2016-08-25 17:46:40 -0500

    ejercicio6

---
+++ js/ejercicio6.js
@@ -0,0 +1,7 @@
+var numero = 0;
+	numero = prompt("Ingrese un número")
+if ((parseInt(numero)%2) == 0) {
+	window.alert("El número " + numero + " es par");
+} else{
+	window.alert("El número " + numero + " es impar");	
+}
